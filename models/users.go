9eccbfc64b03b1b621a1d396aae8d8573652399f
Tonie Victor <victornwanochi@gmail.com>
2025-01-05 23:34:29 +0100

    refactor user repository to use value instead of reference in struct method.

--- models/users.go
+++ models/users.go
@@ -4,37 +4,61 @@ import (
 	"context"
 	"fmt"
 	"time"
-
-	"go.opentelemetry.io/otel"
 )
-
-var tracer = otel.Tracer("models.user")
 
 type UserRepository interface {
 	GetUser(ctx context.Context, field, value string) (User, error)
+	GetUserByID(ctx context.Context, id string) (User, error)
 	DeleteUser(ctx context.Context, id string) error
 	UpdateUser(ctx context.Context, id string, field, value string) (User, error)
-	GetUsers(ctx context.Context) ([]User, error)
+	GetAllUsers(ctx context.Context) ([]User, error)
 	CreateUser(ctx context.Context, username, email, id, avatar string) (User, error)
 }
 
 type User struct {
-	ID        string    `json: "id"`
+	ID        string    `json: "id" validate: "required"`
 	Username  string    `json: "username" validate: "required"`
 	Email     string    `json: "email" validate: "required:email"`
-	Avatar    string    `json: "avatar"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Avatar    string    `json: "avatar" validate: "required"`
+	CreatedAt time.Time `json:"created_at" validate: "required"`
+	UpdatedAt time.Time `json:"updated_at" validate: "required"`
 }
 
-func (u *User) DeleteUser(ctx context.Context, id string) error {
+func (u User) GetUserByID(ctx context.Context, id string) (User, error) {
+	spanctx, span := tracer.Start(ctx, "MODELS getuser by id")
+	defer span.End()
+
+	var user User
+
+	dbctx, cancel := context.WithTimeout(spanctx, dbtimeout)
+	defer cancel()
+
+	query := `SELECT id, username, email, avatar, created_at, updated_at FROM users WHERE id = $1;`
+	row := db.QueryRowContext(dbctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.Avatar,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return User{}, nil
+	}
+
+	return user, nil
+}
+
+func (u User) DeleteUser(ctx context.Context, id string) error {
 	spanctx, span := tracer.Start(ctx, "MODELS delete user")
 	defer span.End()
 
 	dbctx, cancel := context.WithTimeout(spanctx, dbtimeout)
 	defer cancel()
 
-	query := `DELETE from users WHERE id = $1;`
+	query := `DELETE FROM users WHERE id = $1;`
 	_, err := db.ExecContext(dbctx, query, id)
 	if err != nil {
 		return err
@@ -42,7 +66,7 @@ func (u *User) DeleteUser(ctx context.Context, id string) error {
 	return nil
 }
 
-func (u *User) CreateUser(ctx context.Context, username, email, id, avatar string) (User, error) {
+func (u User) CreateUser(ctx context.Context, username, email, id, avatar string) (User, error) {
 	spanctx, span := tracer.Start(ctx, "MODELS create user")
 	defer span.End()
 
@@ -70,7 +94,7 @@ func (u *User) CreateUser(ctx context.Context, username, email, id, avatar strin
 	return user, nil
 }
 
-func (u *User) UpdateUser(ctx context.Context, id string, field, value string) (User, error) {
+func (u User) UpdateUser(ctx context.Context, id string, field, value string) (User, error) {
 	spanctx, span := tracer.Start(ctx, "MODELS update user")
 	defer span.End()
 
@@ -83,8 +107,12 @@ func (u *User) UpdateUser(ctx context.Context, id string, field, value string) (
 	dbctx, cancel := context.WithTimeout(spanctx, dbtimeout)
 	defer cancel()
 
-	query := fmt.Sprintf(`UPDATE users SET %s = $1 WHERE id = $2;`, field)
-	row := db.QueryRowContext(dbctx, query, value, id)
+	query := fmt.Sprintf(`
+		UPDATE users 
+		SET %s = $1
+		SET updated_at = $2
+		WHERE id = $2;`, field)
+	row := db.QueryRowContext(dbctx, query, value, id, time.Now())
 
 	err := row.Scan(
 		&user.ID,
@@ -101,7 +129,7 @@ func (u *User) UpdateUser(ctx context.Context, id string, field, value string) (
 	return user, nil
 }
 
-func (u *User) GetUser(ctx context.Context, field, value string) (User, error) {
+func (u User) GetUser(ctx context.Context, field, value string) (User, error) {
 	spanctx, span := tracer.Start(ctx, "MODELS getuser")
 	defer span.End()
 
@@ -132,7 +160,7 @@ func (u *User) GetUser(ctx context.Context, field, value string) (User, error) {
 	return user, nil
 }
 
-func (u *User) GetUsers(ctx context.Context) ([]User, error) {
+func (u User) GetAllUsers(ctx context.Context) ([]User, error) {
 	spanctx, span := tracer.Start(ctx, "MODELS getusers")
 	defer span.End()
 
